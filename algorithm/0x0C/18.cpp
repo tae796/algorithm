f2b9da195b15d6fd5fd08d943bbc6b63e9464d75
tae796 <45399680+tae796@users.noreply.github.com>
2023-04-09 14:49:45 +0900

    Update 18.cpp

--- algorithm/0x0C/18.cpp
+++ algorithm/0x0C/18.cpp
@@ -1,24 +1,41 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+int arr[2][10];
+int mx = 0;
+int cnt = 0;
 int n;
-int arr[2][8]; // 0:내구도 1:무게
-int ans[8];
-int cnt=0;
+
+void func(int k){
+ if(k==n){
+  mx = max(mx,cnt);
+  return ;
+ }
+ if(cnt == n-1 || arr[0][k]<=0){
+  func(k+1); 
+  return ;
+ }
+ for(int i=0; i<n; i++){
+  if( i == k || arr[0][i]<=0) continue;
+  arr[0][i] -= arr[1][k];
+  arr[0][k] -= arr[1][i];
+  if(arr[0][i] <=0) cnt++;
+  if(arr[0][k] <=0) cnt++;
+  func(k+1);
+  if(arr[0][i] <=0) cnt--;
+  if(arr[0][k] <=0) cnt--;
+  arr[0][i] += arr[1][k];
+  arr[0][k] += arr[1][i];
+ }
+}
 
 
-
-
-
-
-
-
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin >> n;
-
-
-
+int main() {
+ ios::sync_with_stdio(0);
+ cin.tie(0);
+ cin >> n;
+ for(int i=0; i<n; i++) cin >> arr[0][i] >> arr[1][i];
+ func(0);
+ cout << mx;
 }
